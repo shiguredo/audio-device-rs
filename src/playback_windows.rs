83fb1831ef62e99c32f461cba693d7383b7bea6c
melpon <shigemasa7watanabe+github@gmail.com>
2026-06-22 01:50:11 +0900

    cargo fmt

--- src/playback_windows.rs
+++ src/playback_windows.rs
@@ -6,11 +6,12 @@ use std::sync::atomic::{AtomicBool, Ordering};
 use std::thread;
 
 use windows::{
-    Win32::Foundation::*, Win32::Media::Audio::*, Win32::System::Com::*, Win32::System::Threading::*,
+    Win32::Foundation::*, Win32::Media::Audio::*, Win32::System::Com::*,
+    Win32::System::Threading::*,
 };
 
 use crate::common::{AudioDeviceType, AudioFormat, AudioPlaybackConfig, PlaybackFrame};
-use crate::device_windows::{get_device_by_id, SendHandle, SendPtr};
+use crate::device_windows::{SendHandle, SendPtr, get_device_by_id};
 use crate::error::{Error, Result};
 
 struct PlaybackContext {
